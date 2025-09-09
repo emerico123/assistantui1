e6e8d6b959fab6ae5069cedbe57f1ead581f22fe
abishek <abishek@emerico.biz>
2025-09-09 12:59:42 +0800

    10

--- components/assistant-ui/tool-fallback.tsx
+++ components/assistant-ui/tool-fallback.tsx
@@ -3,12 +3,15 @@ import { makeAssistantToolUI } from "@assistant-ui/react";
 export const ToolFallback = makeAssistantToolUI({
   toolName: "get_weather",
   render: ({ args, result, status }) => {
+
+    const location = args.location as string;
+
     if(result)
     if (status.type === "running") return <p>Loading...</p>;
     if (status.type === "incomplete" && status.reason === "error")
       return <p>Error getting weather.</p>;
     if (status.type === "complete"  && result)
-      return <p>{args.location} The weather is.</p>;
+      return <p>{location} The weather is.</p>;
     return null;
   },
 });
